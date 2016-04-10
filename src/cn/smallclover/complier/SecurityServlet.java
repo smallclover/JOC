06e4dfa0a9da0e5f4604bed584f9bdb6fe6eb5b0
smallclover <18363998103@163.com>
2016-04-10 22:06:36 +0800

    JOC的第二个版本
    改用自己编写的bat脚本

--- src/cn/smallclover/complier/SecurityServlet.java
+++ src/cn/smallclover/complier/SecurityServlet.java
@@ -1,23 +1,35 @@
 package cn.smallclover.complier;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+/**
+ * 
+ * @author smallclover
+ *
+ */
 
 @WebServlet("/test.action")
 public class SecurityServlet extends HttpServlet{
 	public Compiler comp = null;
+	public Compiler compi = null;
+	BufferedWriter bw = null;
+	String className = null;
+	String classStr = null;
+	String result = null;	
+	String realpathdir = null;
+	String batPathdir = null;
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		
 		String code = req.getParameter("code");
 		req.setCharacterEncoding("GBK");
@@ -25,36 +37,50 @@ public class SecurityServlet extends HttpServlet{
 		System.out.println(code);
 		PrintWriter out = resp.getWriter();
 		
-		BufferedWriter bw = null;
-		String className = null;//所编译文件的主类类名，即类文件的名字
-		String classStr = null;//获取字符串public class className{
-		String[] classStrArray = null;//将字符串根据空格切分成数组
-		String result = null;
-		classStr = code.substring(code.indexOf("public class"), code.indexOf("{"));
-		classStrArray = classStr.split("\\s{1,}");//通过正则表达式来分割字符串，\s表示空格，{1,}表示匹配至少一个
-		className = classStrArray[classStrArray.length - 1];
-		System.out.println(className);
 		
-		
-		String realpathdir = req.getSession().getServletContext()  
-			     .getRealPath("/temp"); //获取文件存储的真实路径，这里用temp文件夹存储临时编译所用的文件夹
+		realpathdir = req.getSession().getServletContext()  
+				.getRealPath("/temp"); 
 		System.out.println(realpathdir);
 		
-		File sourceFile = new File(realpathdir,className+".java");
-		//如果文件存在
-		if(sourceFile.exists()){
-			sourceFile.delete();
-		}
-		FileWriter fr = new FileWriter(sourceFile);
-		bw = new BufferedWriter(fr);
-		bw.write(code);
-		bw.close();
-		fr.close();
+		batPathdir = req.getSession().getServletContext()  
+				.getRealPath("/bat");
+		System.out.println(batPathdir);
 		try {
-			result = new Complier().ComplierCode(className, realpathdir);
+			validate(code);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+//			result = new Complier().ComplierCode(className, realpathdir);
+			System.out.println("开始编译");
+			result = new ComplierVer2().ComplierCode(className, batPathdir,realpathdir);
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		out.println(result);
 	}
+	
+	private boolean validate(String code) throws IOException{
+		String classStr = null;
+		String[] classStrArray = null;
+		classStr = code.substring(code.indexOf("public class"), code.indexOf("{"));
+		classStrArray = classStr.split("\\s{1,}");
+		className = classStrArray[classStrArray.length - 1];
+		System.out.println(className);
+		
+		File sourceFile = new File(realpathdir,className+".java");
+		if(sourceFile.exists()){
+			sourceFile.delete();
+		}
+        FileWriter fr = new FileWriter(sourceFile);
+        bw = new BufferedWriter(fr);
+        bw.write(code);
+        bw.close();
+        fr.close();
+		return true;
+	}
+	
 }
